9670a8d7993926830a82f5ad84e91232c5d20a00
Leng201202 <6631503129@lamduan.mfu.ac.th>
2025-12-09 12:30:03 +0700

    Remove User,Role,and Auth

--- backend/server/Project/Auth/controllers/auth.js
+++
@@ -1,6 +0,0 @@
-var mongo=require("mongoose");
-var objSchema=require("../../User/models/user.model");
-const createBaseService = require("../../../../helpers/base.service")
-
-const defaultPopulate = []
-module.exports = createBaseService(objSchema, defaultPopulate);
