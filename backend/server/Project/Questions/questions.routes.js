852220221ed1a4a150d1172926372969fa1ce873
Leng201202 <6631503129@lamduan.mfu.ac.th>
2025-12-14 14:31:38 +0700

    Merge Question to Response

--- backend/server/Project/Questions/questions.routes.js
+++ backend/server/Project/Questions/questions.routes.js
@@ -3,11 +3,6 @@ const router = express.Router();
 
 const question = require('./service/question');
 
-<<<<<<< HEAD
-
-=======
-router.get("/id", question.onQuery);
->>>>>>> 1323ecf7565e31d908c5310182fa1b0e09496dd5
 router.get("", question.onQuerys);
 router.post("", question.onCreate);
 router.put("", question.onUpdate);
