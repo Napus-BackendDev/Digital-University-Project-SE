96fd9f4a486e1a868ef20c58ec6586af4d370205
Napus Samuanpho <napus.dev@gmail.com>
2025-12-22 12:02:58 +0700

    refactor: update form model schema to include description and responses fields, and adjust childrenForms handling

--- backend/server/Project/Form/controller/form.js
+++ backend/server/Project/Form/controller/form.js
@@ -3,7 +3,9 @@ var objSchema = require("../models/form.model");
 const createBaseService = require("../../../../helpers/base.service")
 
 const defaultPopulate = [
-    // { path: 'questions' },
+    { path: 'questions' },
+    { path: 'responses' },
+    { path: 'originalFormId' }
 ]
 
 module.exports = createBaseService(objSchema, defaultPopulate);
