ec6c9a635d98edf0c0741da45cd514f4508a43b1
Napus Samuanpho <napus.dev@gmail.com>
2026-01-07 22:56:48 +0700

    refactor: remove unused formScheduler import and clean up form model schema

--- backend/server/Project/Form/models/form.model.js
+++ backend/server/Project/Form/models/form.model.js
@@ -9,15 +9,14 @@ var objSchema = new Schema({
     title : [{ key: { type: String , default: null}, value: { type: String , default: null}}],
     description : [{ key: { type: String , default: null}, value: { type: String , default: null}}],
     questions: { type : [{ type: Schema.Types.ObjectId, ref: 'Questions' }] , default: [] },
-    can_duplicate: { type: Boolean, default: false },
-    status: { type: String, default: STATUS.DRAFT, validate: { validator: v => ALLOW_STATUS.includes(v), message: 'invalid status'} },
+    status: { type: String, ref: 'Setting_Status', default: ''},
     schedule: {
         startAt: { type: Date, default: null },
         endAt: { type: Date, default: null }
     },
     responses: { type: [{ type: Schema.Types.ObjectId, ref: 'Responses' }], default: [] },
-    childrenForms: { type: [{ type: Schema.Types.ObjectId, ref: 'Forms' }] , default: [] },
     originalFormId: { type: Schema.Types.ObjectId, ref: 'Forms', default: null },
+
     // access_control: [{
     //     response: { type: String, default: null }, 
     //     collaborators: [ { userId: { type: Schema.Types.ObjectId, ref: 'Users' }, role: { type: String, default: 'editor' } } ] // editor, viewer
@@ -32,21 +31,8 @@ var objSchema = new Schema({
 
     confirm_message: { 
         message: { type: String, default: 'Your response has been recorded. Thank you!' },
-        link: { type: String, default: null },
      },
     
 }, { timestamps: true });
-
-objSchema.post('save', async function (doc, next) {
-    try {
-        if (!doc.originalFormId) return next();
-        const Form = mongoose.model('Forms');
-
-        await Form.findByIdAndUpdate(doc.originalFormId, { $addToSet: { childrenForms: doc._id } });
-        next();
-    } catch (err) {
-        next(err);
-    }
-})
 
 module.exports = mongoose.model('Forms', objSchema, "Forms");
