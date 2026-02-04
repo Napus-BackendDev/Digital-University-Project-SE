5c658dbfd78873900a1a410d62fb883697ec35c3
Napus Samuanpho <napus.dev@gmail.com>
2026-02-04 14:30:08 +0700

    chore: Install initial project dependencies.

--- frontend/src/components/tabs/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Tab Components - รวม exports
- */
-export { default as QuestionsTab } from './QuestionsTab.vue'
-export { default as ResponsesTab } from './ResponsesTab.vue'
-export { default as SettingsTab } from './SettingsTab.vue'
--- frontend/src/views/FormBuilderView/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * FormBuilderView - รวม exports
- */
-export { default as FormBuilderView } from './FormBuilderView.vue'
-export { default as DataResponses } from './DataResponses.vue'
-export { default as Setting } from './Setting.vue'
