d2d643dfe3677a9c79031c2f7a6c575c4cfcd0c8
Leng201202 <6631503129@lamduan.mfu.ac.th>
2026-03-05 20:23:53 +0700

    Updated api id in form

--- backend/server/Project/Form/form.routes.js
+++ backend/server/Project/Form/form.routes.js
@@ -5,31 +5,31 @@ const form = require('./service/form');
 
 // Get All
 router.get("/exp", function (req, res, next) {
-    req.query.apiId = 0;
+    req.body.apiId = 21;
     next();
 }, form.onQuerys);
 
 // Get One by ID
 router.post("/get", function (req, res, next) {
-    req.query.apiId = 0;
+    req.body.apiId = 22;
     next();
 }, form.onQuery);
 
 // Create
 router.post("", function (req, res, next) {
-    req.query.apiId = 0;
+    req.body.apiId = 23;
     next();
 }, form.onCreate);
 
 // Update
 router.put("", function (req, res, next) {
-    req.query.apiId = 0;
+    req.body.apiId = 24;
     next();
 }, form.onUpdate);
 
 // Delete
 router.delete("", function (req, res, next) {
-    req.query.apiId = 0;
+    req.body.apiId = 25;
     next();
 }, form.onDelete);
 
