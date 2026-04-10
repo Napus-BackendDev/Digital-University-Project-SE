9d3d4cacf544ccd66e5936c19c928d153cedcbf0
Leng201202 <6631503129@lamduan.mfu.ac.th>
2026-04-10 11:07:45 +0700

    Fix Bug in question

--- backend/server/Project/Questions/service/question.js
+++ backend/server/Project/Questions/service/question.js
@@ -3,49 +3,7 @@ const path = require('path');
 const Questions = require('../controller/questions');
 const ResMessage = require("../../Settings/service/message");
 const { getApiId,getSuccessCode } = require("../../../../helpers/apiUtils");
-const { getUploadUrl}= require("../../../../helpers/upload");
-const parseMaybeJson = function (value) {
-    if (typeof value !== 'string') {
-        return value;
-    }
-
-    try {
-        return JSON.parse(value);
-    } catch (err) {
-        return value;
-    }
-};
-
-const isDataUri = function (value) {
-    return typeof value === 'string' && value.startsWith('data:');
-};
-const normalizeQuestionPayload = async function (request) {
-    const rawBody = parseMaybeJson(request.body) || {};
-    const body = parseMaybeJson(rawBody.payload) || rawBody;
-
-    body.title = parseMaybeJson(body.title) || body.title;
-    body.description = parseMaybeJson(body.description) || body.description;
-    body.form = parseMaybeJson(body.form) || body.form;
-    body.type = parseMaybeJson(body.type) || body.type;
-    body.order = parseMaybeJson(body.order) || body.order;
-    body.isRequired = parseMaybeJson(body.isRequired) || body.isRequired;
-    body.nextQuestion = parseMaybeJson(body.nextQuestion) || body.nextQuestion;
-    body.config = parseMaybeJson(body.config) || {};
-
-    if (isDataUri(body.config.image)) {
-        throw new Error('Base64 image is not allowed. Please upload image file instead.');
-    }
-
-    if (request.file) {
-        const uploadedUrl = getUploadUrl(request.file);
-        if (uploadedUrl) {
-            body.config.image = uploadedUrl;
-        }
-    }
-
-    return body;
-};
-
+const { normalizeQuestionPayload } = require('./question.normalize');
 exports.onQuery = async function (request, response) {
     try {
         console.log(`[API ${getApiId(request)}] POST /api/v1/question/get (onQuery)`);
---
+++ backend/server/Project/Questions/service/question.normalize.js
@@ -0,0 +1,48 @@
+const { getUploadUrl } = require('../../../../helpers/upload');
+
+const parseMaybeJson = function (value) {
+    if (typeof value !== 'string') {
+        return value;
+    }
+
+    try {
+        return JSON.parse(value);
+    } catch (err) {
+        return value;
+    }
+};
+
+const isDataUri = function (value) {
+    return typeof value === 'string' && value.startsWith('data:');
+};
+const normalizeQuestionPayload = async function (request) {
+    const rawBody = parseMaybeJson(request.body) || {};
+    const body = parseMaybeJson(rawBody.payload) || rawBody;
+
+    body.title = parseMaybeJson(body.title) || body.title;
+    body.description = parseMaybeJson(body.description) || body.description;
+    body.form = parseMaybeJson(body.form) || body.form;
+    body.type = parseMaybeJson(body.type) || body.type;
+    body.order = parseMaybeJson(body.order) || body.order;
+    body.isRequired = parseMaybeJson(body.isRequired) || body.isRequired;
+    body.nextQuestion = parseMaybeJson(body.nextQuestion) || body.nextQuestion;
+    body.config = parseMaybeJson(body.config) || {};
+
+    if (isDataUri(body.config.image)) {
+        throw new Error('Base64 image is not allowed. Please upload image file instead.');
+    }
+
+    if (request.file) {
+        const uploadedUrl = getUploadUrl(request.file);
+        if (uploadedUrl) {
+            body.config.image = uploadedUrl;
+        }
+    }
+
+    return body;
+};
+
+module.exports = {
+    normalizeQuestionPayload
+    
+};
